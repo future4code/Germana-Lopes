8dc1f9a4006b79f5579bc049ecf4427a4e4c4ab0
germanaetges <germanaetges@gmail.com>
2021-06-14 16:10:16 -0300

    exercício dia 14/06

---
+++ semana1/variaveis/template_javascript/index.js
@@ -0,0 +1,78 @@
+// let a = 10
+// let b = 10
+
+// console.log(b)
+
+// b = 5
+// console.log(a, b)
+
+//vai imprimir 10 5 (primeiro imprime só 10, depois 10 5)
+
+// let a = 10
+// let b = 20
+// let c
+// c = a //10
+// b = c //10
+// a = b //10
+
+// console.log(a, b, c)
+
+// vai imprimir 10 10 10
+
+
+// let p = prompt("Quantas horas você trabalha por dia?")
+// let t = prompt("Quanto você recebe por dia?")
+// alert(`Voce recebe ${t/p} por hora`)
+
+// ao rodar o código notei que let t é dividido por let p e isso gera um resultado que vem do comando após o $. Melhores
+//nomes pras variáveis seria let p horasTrabalhoDia e let t ganhoNoDia
+
+
+// let nome 
+// let idade 
+// console.log(typeof nome)
+// console.log(typeof idade)
+// // ele imprime undefined porque não foi atribuído um valor para a variável
+// nome = prompt("Qual seu nome?")
+// idade = prompt("Qual sua idade?")
+// console.log(typeof nome)
+// console.log(typeof idade)
+// // imprimiu string e string pois comando prompt sempre retorna strig, a não ser que a gente mude
+// console.log("Olá",nome,",você tem",idade,"anos")
+
+
+//exercício 2 
+
+let pergunta1 = "Você está de camiseta?"
+let pergunta2 = "Você está acordada?"
+let pergunta3 = "Você é humana?"
+
+let resposta1 = "Não"
+let resposta2 = "Sim"
+let resposta3 = "Não"
+
+console.log(pergunta1, resposta1)
+console.log(pergunta2, resposta2)
+console.log(pergunta3, resposta3)
+
+
+//exercício 3
+
+let a = 10
+let b = 25
+let c = 10
+a = b
+b = c 
+console.log("O novo valor de a é", a)
+console.log("O novo valor de b é", b) 
+
+
+
+
+
+
+
+
+
+
+
