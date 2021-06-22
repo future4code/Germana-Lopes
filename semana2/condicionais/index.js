70d26eee658a57630b2418017cbc43a9ae758fb6
germanaetges <germanaetges@gmail.com>
2021-06-22 15:28:27 -0300

    resolução dos exercícios de condicionais

---
+++ semana2/condicionais/index.js
@@ -0,0 +1,85 @@
+//EXERCÍCIOS DE INTERPRETAÇÃO
+
+//1. a) ele testa se os números são par ou ímpar. 
+// b) números pares
+// c) números ímpares
+
+
+//Exercício 2
+// a) o código serve pra precificar as frutas conforme
+// a escolha do usuário
+
+//b) O preço da Maçã é R$ 2.25
+
+//c) sem o break ele vai dar o preço do DEFAULT, que é 5 reais.
+// O preço da pera é 5 reais.
+
+// Exercício 3
+//a) está criando um prompt de pergunta pro usuário
+
+// Se for 10 = vai imprimir "Esse número passou no teste"
+// se for -10 não vai imprmir nada pois não foi foi dado o else
+
+//c) Aparecerá um erro no console.log fora da função, 
+// pois "mensagem" não foi definida
+
+// EXERCÍCIOS DE ESCRITA DE CÓDIGO
+
+//1.
+// const qualSuaIdade = Number(prompt("Qual sua idade?"))
+
+// if (qualSuaIdade >= 18) {
+//     console.log("Você pode dirigir!")
+// } else {
+//     console.log("Você não pode dirigir")
+// }
+
+// Exercício 2
+
+// const qualTurno = prompt("Qual turno você estuda? Digite M, V ou N")
+
+// if (qualTurno.toUpperCase() === "M") {
+//  console.log("Bom dia!")
+// } else if (qualTurno.toUpperCase() === "V") {
+//     console.log("Boa tarde!")
+// } else if (qualTurno.toUpperCase() === "N") {
+// //     console.log("Boa noite!")
+// }
+
+// Exercício 3
+
+// const qualTurno = prompt("Qual turno você estuda? Digite M, V ou N")
+
+// switch (qualTurno.toUpperCase()) {
+//     case "M":
+//         console.log("Bom dia!")
+//         break
+//     case "V":
+//         console.log("Boa tarde!")
+//         break
+//     case "N":
+//         console.log("Boa noite!")
+// }
+
+// Exercício 4
+
+// const qualGenero = prompt("Qual o gênero desse filme?")
+// const valorIngresso = Number(prompt("Quanto vai custar o ingresso?"))
+
+// if(qualGenero.toLowerCase() === "fantasia" && valorIngresso < 15){
+//     console.log("Bom filme!")
+// } else {
+//     console.log("Escolha outro filme")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
