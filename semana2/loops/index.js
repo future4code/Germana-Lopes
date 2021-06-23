8ffc3ef566557d9a9efdc04cac398813e661784d
germanaetges <germanaetges@gmail.com>
2021-06-23 15:45:27 -0300

    exercicios mal feitos de loops

---
+++ semana2/loops/index.js
@@ -0,0 +1,45 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+
+//1. vai imprimir 10 pois é a soma dos valores enquanto o 
+//código está sendo executado pq o valor muda também.
+
+//2. a) vai imprimir [19, 21, 23, 25, 27, 30]
+
+// b) sim, basta trocar o if (numero > 18) por
+// (numero = lista[índice a ser pesquisado])
+
+// const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+// for (let numero of lista) {
+//   if (numero = lista[0]) {
+// 		console.log(numero)
+// 	}
+// }
+
+//3. vai imprimir 4 linhas, cada uma com um asterisco a mais pois vai adicionando duplamente o comando
+
+
+// EXERCÍCIO DE ESCRITA DE CÓDIGO:
+
+//1. 
+
+// const perguntaQuantosPets = Number(prompt("Quantos pets você tem?"))
+
+// if(perguntaQuantosPets === 0) {
+//     console.log("Que pena, adote um bichinho!")
+// } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
